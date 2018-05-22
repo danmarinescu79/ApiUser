792227847b7736f492d7a4e4f4901a1ac7cf8f28
Dan Marinescu <dan.marinescu79@icloud.com>
2018-05-22 18:59:25 +0300

    updated user mapper

--- src/ApiUser/Mapper/OAuthUser.php
+++ src/ApiUser/Mapper/OAuthUser.php
@@ -4,7 +4,7 @@
  * @Author: Dan Marinescu
  * @Date:   2018-04-10 14:07:19
  * @Last Modified by:   Dan Marinescu
- * @Last Modified time: 2018-04-12 13:36:10
+ * @Last Modified time: 2018-05-22 18:57:40
  */
 
 namespace ApiUser\Mapper;
@@ -16,9 +16,14 @@ use Zend\Hydrator\ClassMethods;
 class OAuthUser implements \ApiBase\Mapper\MapperJsonInterface
 {
     private $id;
-    private $name;
+    private $first_name;
+    private $last_name;
+    private $created_at;
+    private $created_by;
+    private $updated_at;
+    private $updated_by;
     private $email;
-    private $detail;
+    private $status;
 
     private $objectManager;
     private $hydrator;
@@ -66,19 +71,126 @@ class OAuthUser implements \ApiBase\Mapper\MapperJsonInterface
     /**
      * @return mixed
      */
-    public function getName()
+    public function getFirstName()
     {
-        return $this->name;
+        return $this->first_name;
     }
 
     /**
-     * @param mixed $name
+     * @param mixed $first_name
      *
      * @return self
      */
-    public function setName($name)
+    public function setFirstName($first_name)
     {
-        $this->name = $name;
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     *
+     * @return self
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     *
+     * @return self
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        if (is_object($this->created_by)) {
+            return $this->created_by->getId();
+        }
+
+        return $this->created_by;
+    }
+
+    /**
+     * @param mixed $created_by
+     *
+     * @return self
+     */
+    public function setCreatedBy($created_by)
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     *
+     * @return self
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedBy()
+    {
+        if (is_object($this->updated_by)) {
+            return $this->updated_by->getId();
+        }
+        return $this->updated_by;
+    }
+
+    /**
+     * @param mixed $updated_by
+     *
+     * @return self
+     */
+    public function setUpdatedBy($updated_by)
+    {
+        $this->updated_by = $updated_by;
 
         return $this;
     }
@@ -106,19 +218,19 @@ class OAuthUser implements \ApiBase\Mapper\MapperJsonInterface
     /**
      * @return mixed
      */
-    public function getDetail()
+    public function getStatus()
     {
-        return $this->detail->extract();
+        return $this->status;
     }
 
     /**
-     * @param mixed $detail
+     * @param mixed $status
      *
      * @return self
      */
-    public function setDetail($detail)
+    public function setStatus($status)
     {
-        $this->detail = (new UserDetail($this->objectManager))->hydrate($detail);
+        $this->status = $status;
 
         return $this;
     }
